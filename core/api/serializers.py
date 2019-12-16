8d744bfb1d250a6a74153a8b23cb28562195956a
Victor <victor.acc@live.com>
2019-12-16 00:20:03 -0300

    adding a initial data and serializers changes

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,72 @@
-from rest_framework.serializers import ModelSerializer
-from core.models import BuildPC
+from rest_framework.serializers import ModelSerializer, ValidationError
+from core.models import Processor, Motherboard, Memory, GraphicCard, BuildPC
+
+class ProcessorSerializer(ModelSerializer):
+    class Meta:
+        model = Processor
+        fields = ['product', 'model']
+
+class MotherboardSerializer(ModelSerializer):
+    class Meta:
+        model = Motherboard
+        fields = ['product','support_processor','slots_memory','support_memory', 'integrated_video' ]
+
+class MemorySerializer(ModelSerializer):
+    class Meta:
+        model = Memory
+        fields = ['product','size']
+
+class GraphicCardSerializer(ModelSerializer):
+    class Meta:
+        model = GraphicCard
+        fields = ['product']
 
 class PcBuilderSerializer(ModelSerializer):
+    processor = ProcessorSerializer()
+    motherboard = MotherboardSerializer()
+    memory = MemorySerializer(many=True)
+    graphic_card = GraphicCardSerializer()
+
+    def create_memory(self, memory, pc):
+        for ram in memory:
+            mem = Memory.objects.create(**ram)
+            pc.memory.add(mem)
+
+    def validate(self, attrs):
+        processor = attrs['processor']
+        motherboard = attrs['motherboard']
+        memory = attrs['memory']
+        graphic_card = attrs['graphic_card']
+
+        if processor['model'] != motherboard['support_processor']:
+            raise ValidationError("finish must occur after start")
+
+        return attrs
+
+    def create(self, validated_data):
+        memory = validated_data['memory']
+        del validated_data['memory']
+
+        motherboard = validated_data['motherboard']
+        del validated_data['motherboard']
+
+        processor = validated_data['processor']
+        del validated_data['processor']
+
+        graphic_card = validated_data['graphic_card']
+        del validated_data['graphic_card']
+
+        pc = BuildPC.objects.create(**validated_data)
+        self.create_memory(memory,pc)
+
+        pc.processor = processor
+        pc.motherboard = motherboard
+        pc.graphic_card = graphic_card
+
+        return pc
+
+
     class Meta:
         model = BuildPC
-        fields = ['processor', 'motherboard', 'memory', 'graphic_card']
+        fields = ['client', 'processor', 'motherboard', 'memory', 'graphic_card']
 
--- core/models.py
+++ core/models.py
@@ -1,35 +1,37 @@
 from django.db import models
-
-processor_options = [
-            ( 'Intel', 'Processador Intel Core i5'), 
-            ( 'Intel', 'Processador Intel Core i7'),
-            ('AMD', 'Processador AMD Athlon'),
-            ('AMD', 'Processador AMD Ryzen 7')
-            ]
-motherboard_options = [
-            ('Placa Mãe Asus  Prime','Intel', 2, 16, 'Não')
-            ]
+from django.core.exceptions import ValidationError
 
 class Processor(models.Model):
     product = models.TextField()
     model = models.TextField()
 
+    def __str__(self):
+        return self.product
+
 class Motherboard(models.Model):
     product = models.TextField()
     support_processor = models.TextField()
-    slots_memory = models.IntegerField(max_length=1)
+    slots_memory = models.IntegerField()
     support_memory = models.IntegerField()
     integrated_video = models.BooleanField()
+    
+    def __str__(self):
+        return self.product
 
 class Memory(models.Model):
-    product = models.TextField
+    product = models.TextField()
     size = models.IntegerField()
+
+    def __str__(self):
+        return '%s %dGB' % (self.product, self.size)
 
 class GraphicCard(models.Model):
     product = models.TextField()
 
 class BuildPC(models.Model):
-    processor = models.ForeignKey(Processor,on_delete=models.PROTECT)
+    client = models.TextField()
+    processor = models.ForeignKey(Processor, on_delete=models.PROTECT)
     motherboard = models.ForeignKey(Motherboard,on_delete=models.PROTECT)
     memory = models.ManyToManyField(Memory)
+    qty_memory = models.IntegerField()
     graphic_card = models.ForeignKey(GraphicCard,on_delete=models.SET_NULL, null=True)
