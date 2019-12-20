054db2b149b42786f736419e0a0d3c526b87b184
Victor <victor.acc@live.com>
2019-12-19 22:26:33 -0300

    wsgi change2

--- pc_builder/wsgi.py
+++ pc_builder/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pc_builder.settings")
 
 application = get_wsgi_application()
